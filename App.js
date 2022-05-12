cc1c96ff6c1f8b6dc17487fe5ebff7518f8f3203
andremcqu <mcquade.andre@gmail.com>
2022-05-12 13:24:24 +0200

    I made it look better

--- App.js
+++ App.js
@@ -2,16 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import json from './tp.json';
 
-
+function data() {
+  return json.map((item,key) => {
+    return <View>
+      <Text>{item.title}</Text>
+    </View>
+  })
+}
 export default function App() {
   return (
     <View style={styles.container}>
       <ScrollView>
-{/*this is how to set up a .map {json.map((data) => all func go in this section)} */}
-        {json.map((data) =>
-          <View style={styles.box}>
+        {/*this is how to set up a .map {json.map((data) => all func go in this section)} */}
+        {json.map((data,key) =>
+          <View key={key} style={styles.box}>
             <Image source={{ uri: (data.img) }} style={{ width: 200, height: '100%' }} />
-            <View style={styles.box2}>
+            <View style={styles.doc}>
               <Text style={styles.titles}>{data.title}</Text>
               <Text style={styles.document}>{data.desc}</Text>
             </View>
@@ -27,21 +33,30 @@ const styles = StyleSheet.create({
     flex: 4,
     backgroundColor: '#fff',
     justifyContent: 'center',
+    padding: 5,
+    justifyContent: 'space-between',
+    marginTop: 40,
+    
   },
   box: {
-    flex:4,
+    flex: 4,
     alignItems: 'center',
     flexDirection: 'row',
     padding: 5,
+    backgroundColor: 'oldlace',
+    justifyContent: 'space-between',
+    marginBottom: 6,
   },
   pic: {
     flex: 1,
   },
-  box2: {
+  doc: {
+    padding: 6,
     flex: 4,
+    justifyContent: 'center',
   },
   titles: {
-    justifyContent: 'center'
-    
+    fontWeight: 'bold',
+
   },
 });
